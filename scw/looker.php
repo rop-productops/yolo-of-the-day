2d9d1e6a3c75cf21e3e272419320ed2e072dcf8f
OpenShift guest <steventinwing@gmail.com>
2014-05-08 19:35:10 -0400

    formatting divs

--- scw/looker.php
+++ scw/looker.php
@@ -10,7 +10,46 @@
 
   </head>
   <body>
-
+    
+    
+    
+    <!--BANNER-->
+    <div></div>
+    
+    
+    
+    
+    
+    <!--Executive Quote-->
+    
+    
+    
+    
+    
+    
+    <!--Middle Information-->
+    
+    
+    
+    
+    
+    <!--Map Info-->
+    
+    
+    
+    
+    
+    
+    
+    <!--Last Information-->
+    
+    
+    
+    
+    
+    
+    
+    <!--FOOTER-->
     <div id="crunchbase">
 
       <?php print_crunchbase_div("looker"); ?>
