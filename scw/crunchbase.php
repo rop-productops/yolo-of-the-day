92e749808f675e12fc7797fb0047b6932b2e008b
OpenShift guest <ds.67540@deltaschool.org>
2014-05-08 19:36:05 -0400

    Added curling to the beginning of print_crunchbase_div

--- scw/crunchbase.php
+++ scw/crunchbase.php
@@ -1,14 +1,18 @@
 <?php
 
 function print_crunchbase_div($crunchbase_id) {
-	// get json from crunchbase
-	$json = false;
+	$url = "http://api.crunchbase.com/v/2/organization/looker?user_key=19fe1042062066b50d61f1962cfbd56d";
 
-	// convert json to object
-	$company = false;
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $result = curl_exec($ch);
+
+    $parsed = json_decode($result);
 
 	// call function for specific element
-	$header = crunchbase_div_header($company); 
+	$header = crunchbase_div_header($parsed);
 
 	// print elements
 	print $header;
