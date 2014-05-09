ff7bbc90374142d00e91cf0921efdcf67ca5c873
OpenShift guest <cunakya@gmail.com>
2014-05-08 20:06:52 -0400

    added the View.php file. Changed crunchbase.php to use the View file

--- scw/crunchbase.php
+++ scw/crunchbase.php
@@ -1,18 +1,27 @@
 <?php
 
+include_once("View.php");
+
+
 function print_crunchbase_div($crunchbase_id) {
+
+    $template = new View();
+
 	// get json from crunchbase
 	$json = false;
 
 	// convert json to object
-	$company = false;
+	$company = crunchbase_get_company($json);
 
 	// call function for specific element
 	$header = crunchbase_div_header($company); 
 
 	// print elements
-	print $header;
+	$template->company = $company;
+	$template->header = $header;
 
+    // This file can be found in the templates directory
+    $template->render('crunchbase_view.php');
 }
 
 function crunchbase_div_header($company_object) {
@@ -20,5 +29,8 @@ function crunchbase_div_header($company_object) {
 
 }
 
+function crunchbase_get_company($json){
+    return "company";
+}
 
 ?>
