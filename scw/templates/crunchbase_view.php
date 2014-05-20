08c9bc63a3c546d80694b51c62eb6fba175227b4
OpenShift guest <ds.67540@deltaschool.org>
2014-05-19 20:12:31 -0400

    Added some expanations before the information (Funding: before the number etc)

--- scw/templates/crunchbase_view.php
+++ scw/templates/crunchbase_view.php
@@ -1,15 +1,23 @@
+<html>
+<head>
+</head>
+<body>
+
 <div>
 
-<?php print $this->header ; ?>
-<br>
-<?php print $this->funding ; ?>
-<br>
-<?php print $this->team_total ; ?>
-<br>
-<?php print $this->description ; ?>
-<br>
-<?php print $this->date_founded ; ?>
-<br>
-<?php print $this->company_link ; ?>
+  <?php print $this->header ; ?>
+  <br>
+  Funding: <?php print $this->funding ; ?>
+  <br>
+  Total employees: <?php print $this->team_total ; ?>
+  <br>
+  Description: <?php print $this->description ; ?>
+  <br>
+  Date founded: <?php print $this->date_founded ; ?>
+  <br>
+  Homepage URL: <?php print $this->company_homepage_url ; ?>
 
 </div>
+
+</body>
+</html>
