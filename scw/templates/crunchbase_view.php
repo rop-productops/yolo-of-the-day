892a4f582407a5afdbb288799f5df1268c13a406
prowo <classifiedrandomguy@gmail.com>
2014-05-22 20:14:40 -0400

    fixed ending div and break at end

--- scw/templates/crunchbase_view.php
+++ scw/templates/crunchbase_view.php
@@ -10,4 +10,5 @@
   <?php if($this->description != NULL){print '<div class="small-font"> <span class="font_">Description:</span>'.$this->description.'</div>' ;} ?>
   <br>
   <?php if($this->date_founded != NULL){print '<div class="small-font"> <span class="font_">Date founded:</span>'.$this->date_founded.'</div>'
-  <br>
+  
+</div>
