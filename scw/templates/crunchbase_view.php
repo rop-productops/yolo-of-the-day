7aa6d082efe7f6b790a81baa5bffa413b3f26458
prowo <classifiedrandomguy@gmail.com>
2014-05-22 20:11:13 -0400

    fixed breaks <br> in crunchbase view

--- scw/templates/crunchbase_view.php
+++ scw/templates/crunchbase_view.php
@@ -3,10 +3,11 @@
 
  <div class="Looker"> <?php print $this->header ; ?></div>
   <br>
-  <?php if($this->funding != NULL){print '<div class="small-font fund"> <span class="font_">Funding:</span>'.$this->funding.'</div><br>' ;} ?>
-  <?php if($this->team_total != NULL){print '<div class="small-font employ"> Total employees:'.$this->team_total.'</div><br>' ;} ?>
-  <?php if($this->description != NULL){print '<div class="small-font"> <span class="font_">Description:</span>'.$this->description.'</div><br>' ;} ?>
-  <?php if($this->date_founded != NULL){print '<div class="small-font"> <span class="font_">Date founded:</span>'.$this->date_founded.'</div><br>' ;} ?>
-  <?php if($this->company_homepage_url != NULL){print '<div class="small-font"> <span class="font_">Homepage URL:</span>'.$this->company_homepage_url.'</div><br>' ;} ?>
-
-</div>
+  <?php if($this->funding != NULL){print '<div class="small-font fund"> <span class="font_">Funding:</span>'.$this->funding.'</div>' ;} ?>
+  <br>
+  <?php if($this->team_total != NULL){print '<div class="small-font employ"> Total employees:'.$this->team_total.'</div>' ;} ?>
+  <br>
+  <?php if($this->description != NULL){print '<div class="small-font"> <span class="font_">Description:</span>'.$this->description.'</div>' ;} ?>
+  <br>
+  <?php if($this->date_founded != NULL){print '<div class="small-font"> <span class="font_">Date founded:</span>'.$this->date_founded.'</div>'
+  <br>
