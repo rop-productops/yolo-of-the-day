848acea01a0e0aeed7bd31f90ad957a4045f77cc
prowo <classifiedrandomguy@gmail.com>
2014-05-22 20:17:20 -0400

    fixed ?> ending near line 14

--- scw/templates/crunchbase_view.php
+++ scw/templates/crunchbase_view.php
@@ -9,6 +9,6 @@
   <br>
   <?php if($this->description != NULL){print '<div class="small-font"> <span class="font_">Description:</span>'.$this->description.'</div>' ;} ?>
   <br>
-  <?php if($this->date_founded != NULL){print '<div class="small-font"> <span class="font_">Date founded:</span>'.$this->date_founded.'</div>'
+  <?php if($this->date_founded != NULL){print '<div class="small-font"> <span class="font_">Date founded:</span>'.$this->date_founded.'</div>' ;} ?>
 
-</div><<<<<<< H
+</div><
