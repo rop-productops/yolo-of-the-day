705ec09192b9577651118997717af90f56c8420f
OpenShift guest <bradleybette@gmail.com>
2014-05-29 19:22:25 -0400

    Fixed indenting in the description box

--- scw/templates/crunchbase_view.php
+++ scw/templates/crunchbase_view.php
@@ -6,12 +6,12 @@
    <div class="Looker"> <?php print $this->header ; ?></div>
   <BR />
   <table>
-  <?php if($this->funding != NULL){print '<TR><TD style="width: 225px;" class="small-font fund"> <span class="font_">Funding:</span>'.$this->funding.'</td>' ;} ?>
-  <?php if($this->team_total != NULL){print '<td style="width: 225px;" class="small-font employ"> Total employees:'.$this->team_total.'</td></tr>' ;} ?>
+  <?php if($this->funding != NULL){print '<TR><TD style="width: 225px;" class="small-font fund"> <span class="font_">Funding:</span> <BR />'.$this->funding.'</td>' ;} ?>
+  <?php if($this->team_total != NULL){print '<td style="width: 225px;" class="small-font employ"> Total employees:<BR />'.$this->team_total.'</td></tr>' ;} ?>
   </table>
   <BR />
-  <?php if($this->description != NULL){print '<div class="small-font"> <span class="font_">Description:</span>'.$this->description.'</div>' ;} ?>
+  <?php if($this->description != NULL){print '<div class="small-font"> <span class="font_">Description:</span><BR />'.$this->description.'</div>' ;} ?>
     <BR />
-  <?php if($this->date_founded != NULL){print '<div class="small-font"> <span class="font_">Date founded:</span>'.$this->date_founded.'</div>' ;} ?>
+  <?php if($this->date_founded != NULL){print '<div class="small-font"> <span class="font_">Date founded:</span> <BR />'.$this->date_founded.'</div>' ;} ?>
   
 </div>
