605e2c0b919020ab25c00759b39fc7f071ebe109
OpenShift guest <bradleybette@gmail.com>
2014-05-29 19:27:14 -0400

    Added border

--- scw/templates/crunchbase_view.php
+++ scw/templates/crunchbase_view.php
@@ -3,15 +3,15 @@
  <DIv>
 <!-- Border to see what it is -->
   
-   <div class="Looker"> <?php print $this->header ; ?></div>
+   <div class="Looker border"> <?php print $this->header ; ?></div>
   <BR />
-  <table>
+  <table class="border">
   <?php if($this->funding != NULL){print '<TR><TD style="width: 225px;" class="small-font fund"> <span class="font_">Funding:</span> <BR />'.$this->funding.'</td>' ;} ?>
   <?php if($this->team_total != NULL){print '<td style="width: 225px;" class="small-font employ"> Total employees:<BR />'.$this->team_total.'</td></tr>' ;} ?>
   </table>
   <BR />
-  <?php if($this->description != NULL){print '<div class="small-font"> <span class="font_">Description:</span><BR />'.$this->description.'</div>' ;} ?>
+  <?php if($this->description != NULL){print '<div class="border small-font"> <span class="font_">Description:</span><BR />'.$this->description.'</div>' ;} ?>
     <BR />
-  <?php if($this->date_founded != NULL){print '<div class="small-font"> <span class="font_">Date founded:</span> <BR />'.$this->date_founded.'</div>' ;} ?>
+  <?php if($this->date_founded != NULL){print '<div class="border small-font"> <span class="font_">Date founded:</span> <BR />'.$this->date_founded.'</div>' ;} ?>
   
 </div>
