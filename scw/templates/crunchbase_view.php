19ee0380aa38a43677a9cfb206287f0f2e5f286d
OpenShift guest <bradleybette@gmail.com>
2014-05-29 19:17:12 -0400

    Made changes to table

--- scw/templates/crunchbase_view.php
+++ scw/templates/crunchbase_view.php
@@ -1,19 +1,17 @@
 
 <div class="crunch">
- <Table border=1>
+ <DIv>
 <!-- Border to see what it is -->
-  <Tr>
-   <td class="Looker"> <?php print $this->header ; ?></td>
-  </TR>
-  <TR>
-  <?php if($this->funding != NULL){print '<TD style="width: 50%;" class="small-font fund"> <span class="font_">Funding:</span>'.$this->funding.'</td>' ;} ?>
-  <?php if($this->team_total != NULL){print '<td style="width: 50%;" class="small-font employ"> Total employees:'.$this->team_total.'</td>' ;} ?>
-  </TR>
-  <TR>
-  <?php if($this->description != NULL){print '<td class="small-font"> <span class="font_">Description:</span>'.$this->description.'</td>' ;} ?>
-  </TR>
-  <TR>
-  <?php if($this->date_founded != NULL){print '<td class="small-font"> <span class="font_">Date founded:</span>'.$this->date_founded.'</td>' ;} ?>
-  </TR>
- </Table>
+  
+   <div class="Looker"> <?php print $this->header ; ?></div>
+  <BR />
+  <table>
+  <?php if($this->funding != NULL){print '<TR><TD style="width: 225px;" class="small-font fund"> <span class="font_">Funding:</span>'.$this->funding.'</td>' ;} ?>
+  <?php if($this->team_total != NULL){print '<td style="width: 225px;" class="small-font employ"> Total employees:'.$this->team_total.'</td></tr>' ;} ?>
+  </table>
+  <BR />
+  <?php if($this->description != NULL){print '<div class="small-font"> <span class="font_">Description:</span>'.$this->description.'</div>' ;} ?>
+    <BR />
+  <?php if($this->date_founded != NULL){print '<div class="small-font"> <span class="font_">Date founded:</span>'.$this->date_founded.'</div>' ;} ?>
+  
 </div>
