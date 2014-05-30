13f13d375f2fc5ac38b4ec7ac31e0ef5df74b61e
OpenShift guest <bradleybette@gmail.com>
2014-05-29 20:25:32 -0400

    indenting

--- scw/templates/crunchbase_view.php
+++ scw/templates/crunchbase_view.php
@@ -4,10 +4,24 @@
 <!-- Border to see what it is -->
   
    <div class="Looker border"> <?php print $this->header ; ?></div>
+
+
+
   <table>
-  <?php if($this->funding != NULL){print '<TR><TD style="width: 225px;" class=" border"> <span class="font_">Funding:</span> <BR /> <span class="small-font">'.$this->funding.'</span></td>' ;} ?>
+  
+  
+  <?php if($this->funding != NULL){
+  print '
+    <TR>
+        <TD style="width: 227px;" class=" border"> 
+            <span class="font_">Funding:</span> 
+                <BR /> 
+                    <span class="small-font">
+                        '.$this->funding.'
+                            </span>
+                                </td>' 
+                                    ;} ?>
   <?php if($this->team_total != NULL){print '<td style="width: 225px;" class="border"> <span class="font_">Total employees:</span> <BR /> <span="small-font">'.$this->team_total.'</span> </td></tr>' ;} ?>
-  </table>
   <?php if($this->description != NULL){print '<div class="border small-font"> <span class="font_">Description:</span><BR /> <span class="small-font">'.$this->description.'</span> </div>' ;} ?>
   <?php if($this->date_founded != NULL){print '<div class="border small-font"> <span class="font_">Date founded:</span> <BR /> <span class="small-font">'.$this->date_founded.'</span></div>' ;} ?>
   
