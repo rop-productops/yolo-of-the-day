a151cd1fbb2f030804fc566061f34e86c4f95949
OpenShift guest <julianharrison415@gmail.com>
2014-05-29 20:14:04 -0400

    fixed some issues with the functions

--- scw/DatabaseforSCW.php
+++ scw/DatabaseforSCW.php
@@ -10,6 +10,14 @@ function get_member_entry( $ID){
 
 }
 
+function get_all_member_info(){
+    $database = new DB();
+    $my_query = "select * from Members";
+    $result = $database->query( $my_query )->fetchAll();
+    return ($result);
+
+}
+
 function get_important_people( $ID){
     $database = new DB();
     $my_query = "select * from Significant_Employees where Company_ID = '$ID'";
@@ -21,14 +29,36 @@ function get_important_people( $ID){
 function get_name_id(){
     $database = new DB();
     $my_query = "select ID, Full_Name from Members";
-    $result = $database->query( $my_query )->fetch();
+    $result = $database->query( $my_query )->fetchAll();
     return ($result);
 }
+echo "************************************\n";
 
-print_r (get_member_entry("looker"));
+echo "You should see all the information for Looker\n";
+ 
+ print_r (get_member_entry("looker"));
+ 
+ echo "************************************\n";
+
+echo "You should see all the information all companies\n";
+
+print_r(get_all_member_info());
+ 
+echo "************************************\n";
+
+echo "You should see the names and ID's for all companies\n"; 
 
 print_r (get_name_id());
 
+echo "************************************\n";
+
+echo "You should see all the important people for looker\n";
+
 print_r (get_important_people("looker"));
+
+echo "************************************\n";
+
+
+
 
 ?>
