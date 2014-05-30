395d7b15753513a34528dd55de7cc1176d3eab6b
OpenShift guest <ds.67540@deltaschool.org>
2014-05-29 20:15:39 -0400

    Fixed some indentation in index.php

--- scw/index.php
+++ scw/index.php
@@ -1,21 +1,13 @@
 <?php
 
-    require_once('DatabaseforSCW.php');
+require_once('DatabaseforSCW.php');
     
-    if(!empty($_GET["company"])) {
-
+if(!empty($_GET["company"])) {
     $member_entry= get_member_entry($_GET["company"]);
-        
-         print_r($member_entry);
-         
-    }
-         
-    else
-    {
-    
+    print_r($member_entry);
+} else {
     $name_id=get_name_id($_GET["company"]);
-    
-        print_r($name_id);
-    }
+    print_r($name_id);
+}
 
 ?>
