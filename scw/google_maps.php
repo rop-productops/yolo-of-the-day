6447399a7371e66564ed94bfc78b862dbba580e6
OpenShift guest <steventinwing@gmail.com>
2014-05-29 20:13:26 -0400

    google api set to varible values lat and long

--- scw/google_maps.php
+++ scw/google_maps.php
@@ -6,11 +6,17 @@ include_once("View.php");
 function print_google_map($company_id) {
 
     $template = new View();
+    
+    $lat=37;
+    $lng=-122;
+    
+    $template->lat= $lat;
+    $template->lng= $lng;
+
 
     // This file can be found in the templates directory
     $template->render('google_maps_view.php');
 }
-
 
 
 ?>
