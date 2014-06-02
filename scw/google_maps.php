5335420b4d980e041affb33f46b2ed3bc6b2c9a9
OpenShift guest <steventinwing@gmail.com>
2014-06-02 19:00:21 -0400

    add values for lat and lng

--- scw/google_maps.php
+++ scw/google_maps.php
@@ -1,14 +1,15 @@
 <?php
 
 include_once("View.php");
+include_once("DatabaseforSCW.php");
 
 
 function print_google_map($company_id) {
 
     $template = new View();
     
-    $lat=37;
-    $lng=-122;
+    $lat=get_Long( $ID);
+    $lng=get_Lat( $ID);
     
     $template->lat= $lat;
     $template->lng= $lng;
