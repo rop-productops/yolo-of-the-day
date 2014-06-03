2c198ff9e4ca32592d649db4b795d8992e39914e
OpenShift guest <nicholas.tat@gmail.com>
2014-06-02 20:18:37 -0400

    Created an index2.php file for member page

---
+++ scw/templates/index2.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once("View.php");
+require_once('DatabaseforSCW.php');
+    
+if(!empty($_GET["company"])) {
+    $member_entry= get_member_entry($_GET["company"]);
+
+    $template = new View();
+    $template->id = $member_entry["ID"];
+    $template->name = $member_entry["Full name"];
+
+    $template->render('member_view.php');
+
+    //thanks for a great end of the year all!
+    //you've been really great and motivated and I know that
+    //if you apply yourselves and the things that you've learned
+    //here, you'll become great software engineers!!
+    //Congradulations!
+
+    print_r($member_entry);
+} else {
+    $name_id=get_name_id($_GET["company"]);
+    print_r($name_id);
+}
+
+?>
